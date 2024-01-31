b9af954d6a2c3365abdcacfdb29d8dd6f01002be
Joanna Zhou <joanna@joannas-air.home>
2024-01-30 20:37:04 -0500

    Typescript/Javascript, Map is faster than Object

--- typescript/1049.ts
+++ typescript/1049.ts
@@ -1,19 +1,20 @@
 function lastStoneWeightII(stones: number[]): number {
     const stoneSum = stones.reduce((a, b) => a + b);
-    const target = Math.ceil(stoneSum / 2);
-    const dp = {};
+    const target = Math.floor(stoneSum / 2);
+    const dp = new Map();
 
     function dfs(index, total) {
         const mapkey = `${index},${total}`;
         if (total >= target || index === stones.length) {
-            return Math.abs(total - (stoneSum - total))
+            return Math.abs(2 * total - stoneSum)
         }
-        if (dp[mapkey]) {
-            return dp[mapkey];
-        }
+        if (dp.has(mapkey)) return dp.get(mapkey);
 
-        dp[mapkey] = Math.min(dfs(index + 1, total), dfs(index + 1, total + stones[index]));
-        return dp[mapkey];
+        dp.set(mapkey, Math.min(
+            dfs(index + 1, total),
+            dfs(index + 1, total + stones[index])
+        ));
+        return dp.get(mapkey);
     }
     return dfs(0, 0);
 };
