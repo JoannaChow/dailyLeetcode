d2d5458ab320eab6368561479a466d105467467b
Joanna Zhou <joanna@joannas-air.home>
2024-03-03 17:22:34 -0500

    96, Tree, (with caching) o(n) in time, o(n) in space

--- typescript/1376.ts
+++ typescript/1376.ts
@@ -1,21 +1,21 @@
 function numOfMinutes(
-  n: number,
-  headID: number,
-  manager: number[],
-  informTime: number[]
+    n: number,
+    headID: number,
+    manager: number[],
+    informTime: number[]
 ): number {
-  let time = 0;
-  for (let i = 0; i < n; i++) {
-    time = Math.max(time, bubbleUp(i));
-  }
-  function bubbleUp(employeeID: number): number {
-    const managerID = manager[employeeID];
-    if (managerID !== -1) {
-      manager[employeeID] = -1;
-      informTime[employeeID] += bubbleUp(managerID);
+    let time = 0;
+    for (let i = 0; i < n; i++) {
+        time = Math.max(time, bubbleUp(i));
     }
-    return informTime[employeeID];
-  }
+    function bubbleUp(employeeID: number): number {
+        const managerID = manager[employeeID];
+        if (managerID !== -1) {
+            manager[employeeID] = -1;
+            informTime[employeeID] += bubbleUp(managerID);
+        }
+        return informTime[employeeID];
+    }
 
-  return time;
+    return time;
 }
