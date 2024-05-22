0c5d4cad1679ff7a3a7ceba176328f1580f35000
Joanna Zhou <joanna.chow.jc.jc@gmail.com>
2024-05-22 15:13:48 -0400

    518, DP, top-down [o(n*amount) in time, o(n*amount) in space]  & bottom-up [o(n*amount) in time, o(amount) in space]

--- typescript/518.ts
+++ typescript/518.ts
@@ -1,3 +1,25 @@
+// Top-down
+function change(amount: number, coins: number[]): number {
+    const table = Array.from({ length: coins.length }, () =>
+        Array(amount + 1).fill(-1)
+    );
+    return dfs(0, amount);
+    function dfs(coinIndex: number, amount: number): number {
+        if (amount === 0) return 1;
+        if (coinIndex === coins.length) return 0;
+        if (table[coinIndex][amount] !== -1) return table[coinIndex][amount];
+        if (coins[coinIndex] > amount) {
+            table[coinIndex][amount] = dfs(coinIndex + 1, amount);
+        } else {
+            table[coinIndex][amount] =
+                dfs(coinIndex, amount - coins[coinIndex]) +
+                dfs(coinIndex + 1, amount);
+        }
+        return table[coinIndex][amount];
+    }
+}
+
+/* Bottom-up
 function change(amount: number, coins: number[]): number {
     const dp: number[] = Array(amount + 1).fill(0);
     dp[0] = 1;
@@ -11,3 +33,4 @@ function change(amount: number, coins: number[]): number {
     }
     return dp[amount];
 };
+*/
