ac7aa02afee622e2bddcb6181d4071b4e17bbe0f
Joanna Zhou <joanna.chow.jc.jc@gmail.com>
2024-05-23 21:08:20 -0400

    322, bottom-up, o(n) in time, o(n) in space

--- typescript/322.ts
+++ typescript/322.ts
@@ -1,12 +1,12 @@
 function coinChange(coins: number[], amount: number): number {
-    const minCounts = Array(amount + 1).fill(Infinity);
-    minCounts[0] = 0;
-    for (let amt = 1; amt <= amount; amt++) {
-        coins.forEach(coin => {
-            if (coin <= amt) {
-                minCounts[amt] = Math.min(minCounts[amt], minCounts[amt - coin] + 1);
+    const table = Array(amount + 1).fill(Infinity);
+    table[0] = 0;
+    for (let i = 1; i <= amount; i++) {
+        for (const coin of coins) {
+            if (coin <= i) {
+                table[i] = Math.min(table[i], 1 + table[i - coin]);
             }
-        })
+        }
     }
-    return minCounts[amount] === Infinity ? -1 : minCounts[amount];
-};
+    return table[amount] === Infinity ? -1 : table[amount];
+}
